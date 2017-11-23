1e21a9f3ce68da6c87820ef55e32acaae429d599
Don Pablo <pbourrec@excilys.com>
2017-11-23 16:26:08 +0100

    Version SessionFactory

--- src/main/java/com/excilys/cdb/database/config/ConfigSpring.java
+++ src/main/java/com/excilys/cdb/database/config/ConfigSpring.java
@@ -1,13 +1,12 @@
-package com.excils.cdb.database.config;
+package com.excilys.cdb.database.config;
 
+
+import javax.persistence.EntityManagerFactory;
 
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.Metadata;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.jpa.HibernatePersistenceProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -29,23 +28,23 @@ import com.zaxxer.hikari.HikariDataSource;
 @EnableWebMvc
 @Configuration
 @ComponentScan({"com.excilys.cdb.database.validator",
-				"com.excilys.cdb.database.dao",
-				"com.excilys.cdb.database.ihm",
-				"com.excilys.cdb.database.core",
-				"com.excilys.cdb.database.mapperdto",
-				"com.excilys.cdb.database.mapperdao",
-				"com.excilys.cdb.database.service",
-				"com.excilys.cdb.database.controller",
-				"com.excilys.cdb.database.servlets"})
+	"com.excilys.cdb.database.dao",
+	"com.excilys.cdb.database.ihm",
+	"com.excilys.cdb.database.core",
+	"com.excilys.cdb.database.mapperdto",
+	"com.excilys.cdb.database.mapperdao",
+	"com.excilys.cdb.database.service",
+	"com.excilys.cdb.database.controller",
+"com.excilys.cdb.database.servlets"})
 
 public class ConfigSpring implements WebMvcConfigurer{
 
 	@Autowired  
 	private DatabaseConn databaseConn;
-	
+
 	@Autowired
 	public	HikariDataSource ds;
-	
+
 
 	@Bean
 	public PlatformTransactionManager transactionManager() {
@@ -73,33 +72,16 @@ public class ConfigSpring implements WebMvcConfigurer{
 		viewResolver.setSuffix(".jsp");
 		return viewResolver;
 	}
-	
-	   @Bean
-	   public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-	      LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
-	      em.setDataSource(ds);
-	      
-	      em.setPackagesToScan(new String[] { "com.excilys.cdb.database.core" });
-	      em.setPersistenceProviderClass(HibernatePersistenceProvider.class);
-	      JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-	      em.setJpaVendorAdapter(vendorAdapter);
-	      return em;
-	   }
-	  
-	   @Bean 
-	   public SessionFactory CreateSessionFactory() {
-		   SessionFactory sessionFactory;
-			   try {
-				StandardServiceRegistry standardRegistry = 
-			       new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
-				Metadata metaData = 
-			        new MetadataSources(standardRegistry).getMetadataBuilder().build();
-				 sessionFactory = metaData.getSessionFactoryBuilder().build();
-			   } catch (Throwable th) {
-				System.err.println("Enitial SessionFactory creation failed" + th);
-				throw new ExceptionInInitializerError(th);
-			  }
-			   return sessionFactory;
-			}
-	   }
+
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+		LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+		em.setDataSource(ds);
+
+		em.setPackagesToScan(new String[] { "com.excilys.cdb.database.core" });
+		em.setPersistenceProviderClass(HibernatePersistenceProvider.class);
+		JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+		em.setJpaVendorAdapter(vendorAdapter);
+		return em;
+	}
 }
